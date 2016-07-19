243e73e5f424e0d2680586038fbeba0bad04b67e
Natalie Perna <nmperna@gmail.com>
2016-07-19 19:28:55 -0400

    Ruined the joke.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'http://www.google.ca';
+    protected $redirectTo = '/';
 
     /**
      * Create a new authentication controller instance.
